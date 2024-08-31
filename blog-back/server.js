e39d50123ee1bf1f7bdaba1a350638eeba575e11
Athibdee <ath.145bd@gmail.com>
2024-08-31 23:34:07 +0700

    Testing Basic-Server Port

--- blog-back/server.js
+++ blog-back/server.js
@@ -1,10 +1,26 @@
-const espress = require("express")
+const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
-// asdad
 const mongoose = require("mongoose")
 
 require("dotenv").config()
+
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+// route
+app.get("*",(req,res)=>{
+    res.json({
+        data:"message from server"
+    })
+})
+
+const port = process.env.PORT || 8080
+app.listen(port,()=>console.log(`Start server in port ${port}`))
 
 
 
