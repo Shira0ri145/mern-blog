f51dd24853a2f7a403cc56add0959c5ca1880e32
Athibdee <ath.145bd@gmail.com>
2024-09-05 00:18:12 +0700

    Add fetch all data and show detail blog by slug

--- blog-back/controllers/blogController.js
+++ blog-back/controllers/blogController.js
@@ -1,11 +1,14 @@
 
 const slugify = require("slugify")
 const Blogs = require("../models/blogModel")
+const { v4: uuidv4 } = require('uuid');
 
 // create data
 exports.create=(req,res)=>{
     const {title,content,author}=req.body
-    const slug = slugify(title)
+    let slug = slugify(title)
+
+    if(!slug) slug = uuidv4();
 
     // Validate
     switch(true){
@@ -25,5 +28,39 @@ exports.create=(req,res)=>{
         .catch(err => {
             res.status(400).json({ error: "มีชื่อบทความซ้ำกัน" });
         });
+
+}
+
+// Fetch all data
+exports.getAllblogs = (req, res) => {
+    Blogs.find({})
+        .then(blogs => {
+            res.json(blogs);
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message });
+        });
+};
+
+
+// Slugify for blog
+exports.singleBlog=(req,res)=>{
+    const {slug} = req.params
+    Blogs.findOne({ slug }).exec()
+    .then(blog => {
+        if (!blog) {
+            // If no blog is found, send a 404 response
+            return res.status(404).json({ message: 'Blog not found' });
+        }
+        // Return the blog as JSON
+        res.json(blog);
+    })
+    .catch(err => {
+        // Handle errors and send a 500 response
+        // console.error('Error fetching blog:', err);
+        res.status(500).json({ error: err.message });
+    });
+
+    
 
 }
