28cb263b44c0dd4c3a1aa88b96954ccfed9d55d9
Athibdee <ath.145bd@gmail.com>
2024-09-06 02:01:20 +0700

    Add authen expressjwt for api

--- blog-back/routes/blog.js
+++ blog-back/routes/blog.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const router = express.Router()
-const {create,getAllblogs,singleBlog,deleteBlog,updateBlog} =require("../controllers/blogController")
+const { create,getAllblogs,singleBlog,deleteBlog,updateBlog } =require("../controllers/blogController")
+const { requireLogin } = require("../controllers/authController")
 
-router.post('/create',create)
+
+router.post('/create',requireLogin,create)
 router.get('/blogs',getAllblogs)
 router.get('/blog/:slug',singleBlog)
-router.delete('/blog/:slug',deleteBlog)
+router.delete('/blog/:slug',requireLogin,deleteBlog)
 
-router.put('/blog/:slug',updateBlog)
+router.put('/blog/:slug',requireLogin,updateBlog)
 
 module.exports=router
